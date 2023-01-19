86f9f78cad441bcb86128284449a96888a858830
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-01-19 18:29:13 +0600

    add mongodb

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 
+require('dotenv').config()
+
+const cors = require('cors');
+app.use(cors());
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yjrwkkd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database connected');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+console.log(uri)
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Doctor app listening on port ${port}`)
 })
